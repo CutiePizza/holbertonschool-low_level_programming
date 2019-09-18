aa2ae1e2943d858581392df6a98d02ba600c7430
CutiePizza <ines.chokri97@gmail.com>
2019-09-18 13:40:55 +0000

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
+/**
+* main - Entry point
+*main
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	
 puts("\"Programming is lilke building a multilingual puzzle");
-
-	return(0);
-	
-	
-	
+return (0);
 }
