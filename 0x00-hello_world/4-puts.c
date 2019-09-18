b4444cb768780ebf1dbe5f183df7ddedaaba9a40
CutiePizza <ines.chokri97@gmail.com>
2019-09-18 13:35:19 +0000

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
 int main(void)
 {
 	
-char str1[]="Programming is lilke building a multilingual puzzle";
+puts("\"Programming is lilke building a multilingual puzzle");
 
-	puts(str1);
 	return(0);
 	
 	
