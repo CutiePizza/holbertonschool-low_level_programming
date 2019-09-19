77c58874c31e16dc4317d3761e1a1cdc353f1eee
CutiePizza <ines.chokri97@gmail.com>
2019-09-19 16:44:53 +0000

    first push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Inès Chokri
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
