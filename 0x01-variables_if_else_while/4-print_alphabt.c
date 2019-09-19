d49f367b44ee0e29bf9b1a3a24b72089979fcc9b
CutiePizza <ines.chokri97@gmail.com>
2019-09-19 15:47:48 +0000

    ok

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
+* Inès Chokri
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-char i;
-
+{int i;
 for (i = 'a'; i < 'z'; i++)
-if (i != 'q' && i != 'e')
+if (i != 'e' && i != 'q')
 putchar(i);
 putchar('\n');
 return (0);
