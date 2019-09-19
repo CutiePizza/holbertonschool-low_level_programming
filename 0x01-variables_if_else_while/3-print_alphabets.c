f98c009d908f40c35aa86ab650244bf35124293b
CutiePizza <ines.chokri97@gmail.com>
2019-09-19 13:07:32 +0000

    correct

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i,y;
+for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+for (y = 'A'; y <= 'Z'; y++)
+	putchar(y);
+	putchar('\n');
+return (0);
+}
