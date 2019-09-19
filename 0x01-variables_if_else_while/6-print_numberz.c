38bc9c618144d79db4a33911616c9d65d0f76bf8
CutiePizza <ines.chokri97@gmail.com>
2019-09-19 16:27:39 +0000

    push one

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Inès Chokri
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
