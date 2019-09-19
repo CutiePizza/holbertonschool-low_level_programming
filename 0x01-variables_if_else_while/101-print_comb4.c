dd818c37c91af631d46d5f9db21955b4ee646e13
CutiePizza <ines.chokri97@gmail.com>
2019-09-19 19:49:57 +0000

    Commit 1

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Inès Chokri
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i,j,k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i < 8 || j < 9 || k < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+
+}
