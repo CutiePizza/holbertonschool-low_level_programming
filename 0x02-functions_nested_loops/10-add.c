5b675ed5484d31952cd19f1a2318bdc8e0a077c2
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 11:20:47 +0000

    ok

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-
+/**
+* add - check the code for Holberton School students.
+*@a: integer
+*@b: integer
+* Return: Always 0.
+*/
 int add(int a, int b)
 {
 	return (a + b);
