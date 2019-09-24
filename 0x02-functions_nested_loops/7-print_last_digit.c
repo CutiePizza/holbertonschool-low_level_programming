028ddb558862f90a41864b8544090c4359959ecf
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 10:03:09 +0000

    second commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+* print_last_digit - check the code for Holberton School students.
+    *@c: integer
+     * Return: Always c.
+      */
 int print_last_digit(int c)
 {
 if (c < 0)
