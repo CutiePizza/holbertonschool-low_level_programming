0ec48f3a2047d6aa8fe8bf86805ad5e8d3d2a02c
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 21:05:29 +0000

    ok2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,27 +1,40 @@
 #include "holberton.h"
-
+/**
+* times_table - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 void times_table(void)
 {
-int i, j;
+int i, j, s;
 
 for (i = 0; i < 10; i++)
 {
-	for (j = 0; j < 10; j++)
-	{
-int s = i*j;
-if (s / 10 != 0 )
+for (j = 0; j < 10; j++)
 {
+s = i * j;
+if (s >= 10)
+{
+_putchar(' ');
 _putchar(s / 10 + '0');
-}
 _putchar(s % 10 + '0');
-if ( j != 9 )
+if (j != 9)
+_putchar(',');
+}
+else
+{
+if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(s + '0');
+if (j != 9)
 {
 _putchar(',');
-_putchar(' ');
+}
 }
 }
 _putchar('\n');
 }
-
 }
-
