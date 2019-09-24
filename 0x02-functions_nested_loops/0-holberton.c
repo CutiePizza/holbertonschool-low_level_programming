23b37ac78a1dcce283e00d9357bd0dda1ce0b800
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 07:51:00 +0000

    first commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i[] = "Holberton";
+int j = 0;
+
+while (j < 10)
+{
+_putchar(i[j]);
+j++;
+}
+_putchar('\n');
+
+return (0);
+}
