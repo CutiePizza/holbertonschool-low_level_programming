9b421fbacaaf4caca93071e103a4b824e4f6925f
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 10:00:02 +0000

    ok

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
-
-int _abs(int n) 
-{	if (n > 0)
-	return (n);
-	else
-	return (-n);
-	
+/**
+* _abs - return abs of n
+*@n: integer
+* Return: Always n..
+*/
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else
+return (-n);
 }
