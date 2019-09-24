a370443ff14db21eae46415da5eb67fac73112c9
CutiePizza <ines.chokri97@gmail.com>
2019-09-24 09:48:26 +0000

    second commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
-* main - check the code for Holberton School students.
-*
+* print_sign - check the code for Holberton School students.
+*@n: integer
 * Return: Always 0.
 */
 int print_sign(int n)
