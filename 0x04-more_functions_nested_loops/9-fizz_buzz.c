c549608f328f49aba330d3b666a609c82572dae4
CutiePizza <ines.chokri97@gmail.com>
2019-09-26 17:47:11 +0000

    fixed tabs

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,44 +6,27 @@
  */
 int main(void)
 {
-int i, a, b, s;
+int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-	if (i < 10)
-	{
-		if ((i == 3 || i == 6 || i == 9) && (i == 5 || i == 0))
-			printf("FizzBuzz ");
-		else if (i == 3 || i == 6 || i == 9)
-			printf("Fizz ");
-		else if (i == 5)
-			printf("Buzz ");
-		else
-			printf("%d ", i);
-	}
-	else if (i < 100)
-	{
-	a = i / 10;
-	b = i % 10;
-	s = a + b;
-		while (s > 10)
-		{
-		a = a / 10;
-		b = b % 10;
-		s = a + b;
-		}
-		if (((a + b == 3) || (a + b == 6) || (a + b == 9)) && (b == 0 || b == 5))
-			printf("FizzBuzz ");
-		else if ((a + b == 3) || (a + b == 6) || (a + b == 9))
-			printf("Fizz ");
-		else if (i % 10 == 0 || i % 10 == 5)
-			printf("Buzz ");
-		else
-			printf("%d ", i);
-	}
-	else if (i == 100)
-		printf("Buzz ");
-	}
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+	printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+	printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+	printf("Buzz ");
+}
+else
+{
+	printf("%d ", i);
+}
+}
 printf("\n");
 return (0);
 }
