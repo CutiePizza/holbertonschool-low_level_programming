d443e83b2b3fe7a55e70ae33f89c6b52fc9df4bb
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 10:36:38 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* print_array - check the code for Holberton School students.
+*@a: integer
+*@n: integer
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{	
+		if (i != n - 1)
+		printf("%d, ", *(a + i));
+		else
+		printf("%d", *(a + i));
+	}
+	printf("\n");
+}
