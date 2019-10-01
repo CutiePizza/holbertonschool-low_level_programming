98666651e64b034a346b8bc1bee331964773533c
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 12:36:51 +0000

    betty fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,10 +3,8 @@
 *@s: character
 * Return: Always 0.
 */
-
 int _atoi(char *s)
 {
-	
 	int l;
 	int i, c, m;
 	int ok;
@@ -16,36 +14,30 @@ int _atoi(char *s)
 	m = 1;
 	l = 0;
 	ok = 0;
-
 	while (s[l] != '\0')
 	{
 		l++;
 	}
-
-	while (i < l && ok == 0) 
+	while (i < l && ok == 0)
 	{
-	
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			c = c * 10 + (s[i] - '0');
-			
 				if (s[i + 1] >= '0' && s[i + 1] <= '9')
 					ok = 0;
-				else 
+				else
 					ok = 1;
 		}
 		if (s[i] == '+')
 		{
-			m = m * 1;	
+			m = m * 1;
 		}
-		else if(s[i] == '-')
+		else if (s[i] == '-')
 		{
 			m = m * -1;
 		}
-	
 		i++;
 	}
-
-return (c * m);		
+return (c * m);
 }
 
