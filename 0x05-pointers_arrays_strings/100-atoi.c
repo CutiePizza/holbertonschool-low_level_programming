401b2e40e97bccaf0523c45af20a4b0d43b41b06
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 21:14:56 +0000

    ok

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,20 @@
 */
 int _atoi(char *s)
 {
-
 	int i, c, sign;
 
 	sign = 1;
 	i = 0;
 	c = 0;
-
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
 	{
-		if (s[i] == '-')
+		if (*(s + i) == '-')
 			sign *= -1;
 		i++;
 	}
-	while ((s[i] >= '0') && (s[i] <= '9'))
+	while ((*(s + i) >= '0') && (*(s + i) <= '9'))
 	{
-		c = c * 10 + (s[i] - '0');
+		c = c * 10 + (*(s + i) - '0');
 		i++;
 	}
 return (c * sign);
