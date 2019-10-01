ba72b42fd31aa46a02bbb7a15c5c51924c10fd4d
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 09:14:08 +0000

    first commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,8 +6,7 @@
 */
 void rev_string(char *s)
 {
-	int i;
-	int j;
+	int i, j, k;
 	char n;
 
 	i = 0;
@@ -17,11 +16,15 @@ while (*(s + i) != '\0')
 	i++;
 }
 i--;
-for (j = 0; j != i; j++)
+k = i / 2;
+j = 0;
+
+while (j != k)
 {
-	n = *(s + j);
-	*(s + j) = *(s + i);
-	*(s + i) = n;
-	i--;
+n = *(s + j);
+*(s + j) = *(s + i);
+*(s + i) = n;
+k--;
+i--;
 }
 }
