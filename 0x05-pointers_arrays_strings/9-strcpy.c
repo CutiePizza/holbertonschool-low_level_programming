6f2ac098f0420f01052edffb150346ae29584a4f
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 11:15:59 +0000

    ok

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strcpy - check the code for Holberton School students.
+*@dest: char
+*@src: char
+* Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+
+	int l, j;
+
+	l = 0;
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	for (j = 0; j < l; j++)
+	{
+		dest[j] = src[j];
+	}
+
+return (dest);
+}
