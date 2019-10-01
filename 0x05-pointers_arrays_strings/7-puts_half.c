93c8f11953794ff4686f14398f449bad2e8e5a2a
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 10:21:13 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a string
+ *@str: string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int len, j, n;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+if (len % 2 == 0)
+{
+	for (j = (len / 2); j < len; j++)
+	{
+		_putchar(str[j]);
+	}
+}
+else
+{
+n = (len - 1) / 2;
+	for (j = n; j < len; j++)
+	{
+		_putchar(str[j]);
+	}
+
+}
+_putchar('\n');
+}
