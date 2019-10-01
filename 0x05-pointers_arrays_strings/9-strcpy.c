b954d595b1cee31f414d8ec69e9afa36a641316a
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 11:30:23 +0000

    ok

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 	int l, j;
 
 	l = 0;
-	while (src[l] >= '\0')
+	while (src[l] != '\0')
 	{
 		l++;
 	}
@@ -20,6 +20,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[j] = src[j];
 	}
+	dest[j + 1] = '\0';
 
 return (dest);
 }
