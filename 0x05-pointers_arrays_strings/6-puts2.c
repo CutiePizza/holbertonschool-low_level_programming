9a628bc937d67b234b44390241fcadd0bbe0e5da
CutiePizza <ines.chokri97@gmail.com>
2019-10-01 10:08:47 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * puts2 - return odd lines
+ *@str: character
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int len, j;
+	
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	for (j = 0; j <= len; j++)
+	{
+		if (j % 2 == 0)
+		{
+		_putchar(str[j]);
+		}
+	}
+_putchar('\n');
+}
