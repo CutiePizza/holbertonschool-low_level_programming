ef2f6afa155eecf3c29ff681ed3cc38d3a775401
CutiePizza <ines.chokri97@gmail.com>
2019-10-03 14:49:19 +0000

    push task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize string
+ * @s: string
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == '\n' || s[i] == '\t' || s[i] == ' ' || s[i] == '.'
+		    || s[i] == ';' || s[i] == ',' || s[i] == '!' || s[i] == '?'
+		    || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
+		}
+	}
+	return (s);
+}
