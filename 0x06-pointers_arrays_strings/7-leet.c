7fecf5a55f21f27caa9994ccd95dab3ccdf936f1
CutiePizza <ines.chokri97@gmail.com>
2019-10-03 19:11:42 +0000

    push task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * leet - check the code for Holberton School students.
+ * @ch: character
+ * Return: Always 0.
+ */
+
+char *leet(char *ch)
+{
+
+int i, j;
+char T[] = "aAeEoOtTlL";
+char T2[] = "4433007711";
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		for (j = 0; T[j] != '\0'; j++)
+		{
+			if (ch[i] == T[j])
+				ch[i] = T2[j];
+		}
+
+	}
+return (ch);
+
+}
