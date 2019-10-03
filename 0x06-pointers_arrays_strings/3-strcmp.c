c9c647ebcce248568edfc638a83531ecfd42d95f
CutiePizza <ines.chokri97@gmail.com>
2019-10-03 13:17:33 +0000

    push task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strcmp - check the code for Holberton School students.
+ * @s1: char
+ * @s2: char
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	int l1 = 0, l2 = 0, c = 0, sup, i = 0;
+
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	if (l1 > l2)
+	sup = l2;
+	else
+	sup = l1;
+
+	while (i < sup && c == 0)
+	{
+		c = s1[i] - s2[i];
+		i++;
+	}
+
+	return (c);
+}
