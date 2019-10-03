35324b74c39eecbf2dc9e468072889af1f753899
CutiePizza <ines.chokri97@gmail.com>
2019-10-03 21:40:23 +0000

    ok

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,17 +8,17 @@
 char *rot13(char *ch)
 {
 
-int i;
+int i, j;
+char T[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char I[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; ch[i] != '\0'; i++)
 	{
-
-		if ((ch[i] >= 'a' && ch[i] <= 'm') || (ch[i] >= 'A' && ch[i] <= 'M'))
+		for (j = 0; j < 52; j++)
 		{
-		ch[i] = ch[i] + 13;
+			if (T[j] == ch[i])
+				ch[i] = I[j];
 		}
-		else if ((ch[i] >= 'n' && ch[i] <= 'z') || (ch[i] >= 'N' && ch[i] <= 'Z'))
-		ch[i] = ch[i] - 13;
 	}
 return (ch);
 }
