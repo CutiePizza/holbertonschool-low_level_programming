980e1a2abd2a8f9e274ce31fb45d95ac7cae0c8d
CutiePizza <ines.chokri97@gmail.com>
2019-10-06 10:04:49 +0000

    fixed long to long int

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,19 +6,18 @@
 */
 int main(void)
 {
-long  n_1 = 2;
-long  n_2 = 1;
-long  i, s;
-
-s = 2;
+long int n_1 = 2;
+long int n_2 = 1;
+long int s = 2;
+int i;
 
 	for (i = 1; i < 31; i++)
 	{
 		if ((n_1 + n_2) % 2 == 0)
 		s = n_1 + n_2;
 
-		n_2 =  n_1;
 		n_1 = n_1 + n_2;
+		n_2 = n_1 - n_2;
 	}
 	printf("%ld\n", s);
 return (0);
