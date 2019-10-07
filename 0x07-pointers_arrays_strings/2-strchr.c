cd08c3eca743e7cbc25483d8740582715574bee5
CutiePizza <ines.chokri97@gmail.com>
2019-10-07 14:24:51 +0000

    ok

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,13 @@ char *_strchr(char *s, char c)
 
 	int i, j = 0;
 
-	while (*(s + j) != '\0')
+	while (s[j]!= '\0')
 		j++;
 
 	for (i = 0; i < j; i++)
 	{
-		if (*(s + i) == c)
-			return (&(*(s + i)));
+		if (s[i] == c)
+			return (&(s[i]));
 	}
 
 	return (0);
