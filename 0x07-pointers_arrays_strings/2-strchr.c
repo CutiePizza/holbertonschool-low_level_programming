dbc8d6b007f8dc02da94a4649d1233ea9ffdae29
CutiePizza <ines.chokri97@gmail.com>
2019-10-07 14:12:20 +0000

    ok

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * _strchr - locates a chatracter in a string
+  * @s: character
+  * @c: character
+  * Return: pointer to s
+  */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return ((char *)0);
+}
