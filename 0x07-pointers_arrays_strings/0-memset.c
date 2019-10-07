5d4e17ff2d2c5871a30faed3d7b2eb5db8e491d0
CutiePizza <ines.chokri97@gmail.com>
2019-10-07 13:00:23 +0000

    push task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills the first n bytes of the area pointed to by s
+ * @s: character
+ * @b: character
+ * @n: integer
+ * Return: A pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+
+}
