02f8728c9ba12684929b1c5fa4b800c6267c9c9c
CutiePizza <ines.chokri97@gmail.com>
2019-10-09 14:36:29 +0000

    first push of task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,33 @@
 #include "holberton.h"
 
 /**
+  * _sqrt_recursion - Call the function that calculates the sqrt
+  * @n: integer
+  * Return: integer
+  */
+
+int _sqrt_recursion(int n)
+{
+	return (real_sqrt_recursion(0, n));
+}
+
+/**
+  * real_sqrt_recursion - Calculates the sqrt of a number
+  * @i: integer
+  * @n: integer
+  * Return: integer
+  */
+
+int real_sqrt_recursion(int i, int n)
+{
+
+	if (i * i == n)
+		return (i);
+	else if (i < n)
+		{
+		return (real_sqrt_recursion(i + 1, n));
+		}
+	else
+		return (-1);
+
+}
