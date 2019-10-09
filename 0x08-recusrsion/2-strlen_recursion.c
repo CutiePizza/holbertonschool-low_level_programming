ae611443cfc267fc27d9e1d9a1e17b2700205cc0
CutiePizza <ines.chokri97@gmail.com>
2019-10-09 11:22:40 +0000

    fisrt push tasl 2

---
+++ 0x08-recusrsion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _strlen_recursion - Calculates the length of a given String
+  * @s: String
+  * Return: An integer
+  */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+
+		return (1 + _strlen_recursion(s + 1));
+
+
+}
