353aef77341cda0740b21bb19077e86c7ebf0400
CutiePizza <ines.chokri97@gmail.com>
2019-10-11 18:35:28 +0000

    pushing task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - adds numbers
+  * @argc: number of arguments
+  * @argv: arguments
+  * Return: 1 or 0.
+  */
+
+int main(int argc, char *argv[])
+{
+
+	int sum = 0, i, n;
+
+	if (argc - 1 == 0)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+		if (atoi(argv[i]))
+		{
+		n = atoi(argv[i]);
+		sum += n;
+		}
+		else
+		{
+		printf("error\n");
+		return (1);
+		}
+		}
+	}
+printf("%d\n", sum);
+return (0);
+}
