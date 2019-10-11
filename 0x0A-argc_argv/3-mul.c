e21b6cf57b40a70e780ecc65d01ab8494b8209f4
CutiePizza <ines.chokri97@gmail.com>
2019-10-11 18:21:27 +0000

    pushing task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Program that multiplies two numbers
+  * @argc: number of arguments
+  * @argv: arguments
+  * Return: 0.
+  */
+
+int main(int argc, char *argv[])
+{
+
+	int num1, num2, res;
+
+	if (argc - 1 <= 1 || argc - 1 > 2)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		res = num1 * num2;
+		printf("%d\n", res);
+	}
+	return (0);
+
+}
