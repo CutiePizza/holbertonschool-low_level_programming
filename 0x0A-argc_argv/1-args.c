efe66550b3bf77dab8549f5931d4f112ec84b697
CutiePizza <ines.chokri97@gmail.com>
2019-10-11 17:15:39 +0000

    Pushing task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints number of rguments passed to main
+  * @argc: number of arguments
+  * @argv: arguments
+  * Return: 0.
+  */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
