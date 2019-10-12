b0c6ad4e84b9f7ab5af0633c752a94c29e0ed85f
CutiePizza <ines.chokri97@gmail.com>
2019-10-12 13:41:24 +0000

    fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+/**
+  * is_positive - check if parameter is postive number
+  * @ch: string
+  * Return: 0 if true, 1 if not
+  */
+
+int is_positive(char *ch);
 /**
   * main - adds numbers
   * @argc: number of arguments
@@ -13,29 +21,36 @@ int main(int argc, char *argv[])
 
 	int sum = 0, i, n;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else if (argc > 1)
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-		if (*argv[i] == '0')
-			sum += 0;
-		else
+		if (is_positive(argv[i]) == 0)
 		{
 		n = atoi(argv[i]);
-		if (n > 0)
 		sum += n;
+		}
 		else
 		{
 		printf("Error\n");
 		return (1);
 		}
 		}
-		}
-		printf("%d\n", sum);
 	}
+		printf("%d\n", sum);
 return (0);
+}
+
+int is_positive(char *ch)
+{
+	int i = 0;
+
+	while (ch[i] != '\0')
+	{
+		if (isdigit(ch[i]) == 0)
+			return (1);
+		i++;
+	}
+	return (0);
+
 }
