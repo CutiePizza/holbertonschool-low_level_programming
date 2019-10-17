49925abf895f5762eca0e19eca153359b53cdd56
CutiePizza <ines.chokri97@gmail.com>
2019-10-17 21:23:57 +0000

    add new files

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _realloc - Reallocates memory block
+  * @ptr: previous pointer
+  * @old_size: old size of previous pointer
+  * @new_size: new size for our pointer
+  * Return: New resized Pointer
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+
+
+
+
+
+
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - program that multiply two numbers
+  * @argc: number of aruments
+  * @argv: arguments
+  * Return: 0
+  */
+
+int main(int argc, char *argv)
+{
+
+
+
+
+
+return (0);
+}
