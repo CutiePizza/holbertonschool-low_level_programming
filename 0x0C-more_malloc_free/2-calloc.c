5cbff259cc7eb9e61be57767ecf2606fc227af4f
CutiePizza <ines.chokri97@gmail.com>
2019-10-17 19:10:56 +0000

    Done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
 
 	return (p);
 }
