e52a2f23ccf1968faf357a32857077be515900ca
CutiePizza <ines.chokri97@gmail.com>
2019-10-17 20:16:58 +0000

    done3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,8 +19,11 @@ int *array_range(int min, int max)
 	p = malloc(sizeof(int) * size);
 	if (p == NULL)
 		return (NULL);
-	for (i = min; i < size; i++)
-		p[i] = i;
+	for (i = 0; i < size; i++)
+	{
+		p[i] = min;
+		min++;
+	}
 return (p);
 
 }
