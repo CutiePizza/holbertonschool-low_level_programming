1be984f1f8fedbe879d6fe84393b09423842a83a
CutiePizza <ines.chokri97@gmail.com>
2019-10-21 13:48:52 +0000

    task 2 pushed

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog - prints a struct dog
+  * @d: Variable of type struct d
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+
+	if (d->name == 0)
+		printf("Name: %s\n", "nil");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("Age: %.1f\n", d->age);
+	else
+		printf("Age: %.1f\n", d->age);
+
+	if (d->owner == 0)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _DOG_H
 #define _DOG_H
-
 /**
   * struct dog - Define a structure that contains a dog information
   * @name: Name of the dog, String
@@ -17,4 +16,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /*_DOG_H */
