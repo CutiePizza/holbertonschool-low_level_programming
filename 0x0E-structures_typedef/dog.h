20dbbbd6732cc09260ec1c15ad6bb0db69f699e8
CutiePizza <ines.chokri97@gmail.com>
2019-10-21 12:15:49 +0000

    task 0 pushed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  * struct dog - Define a structure that contains a dog information
+  * @name: Name of the dog, String
+  * @age: Age of the dog, float
+  * @owner: Dog's owner, String
+  *
+  * Description: No need for a longer description.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_DOG_H */
