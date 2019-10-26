357610e90e0ba9d243fd88e87ab653e152c11883
CutiePizza <ines.chokri97@gmail.com>
2019-10-26 17:16:48 +0000

    ok

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,15 +2,59 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include "variadic_functions.h"
-
 /**
-   * print_all - print all the parameters
-    * @format: constant pointer to char
-     */
+ * print_c - print char
+ * @c: list
+ */
+
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+/**
+ * print_i - print char
+ * @i: list
+ */
+
+
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+/**
+ * print_f - print char
+ * @f: list
+ */
+
+
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+/**
+ * print_s - print char
+ * @s: list
+ */
+
+
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str != NULL)
+	{
+		printf("%s", str);
+		return;
+	}
+	printf("(nil)");
+}
+/**
+ * print_all - print all the parameters
+ * @format: constant pointer to char
+ */
 
 void print_all(const char * const format, ...)
 {
-
 	t array[] = {
 		{"c", print_c},
 		{"i", print_i},
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -18,51 +18,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
-  * print_c - print char
-  * @c: list
-  */
-
-void print_c(va_list c)
-{
-	printf("%c", va_arg(c, int));
-}
-/**
-  * print_i - print char
-  * @i: list
-  */
-
-
-void print_i(va_list i)
-{
-	printf("%d", va_arg(i, int));
-}
-/**
-  * print_f - print char
-  * @f: list
-  */
-
-
+/*void print_c(va_list c);
+void print_i(va_list i);
 void print_f(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-/**
-  * print_s - print char
-  * @s: list
-  */
-
-
-void print_s(va_list s)
-{
-	char *str = va_arg(s, char *);
-
-	if (str != NULL)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
-}
+void print_s(va_list s);*/
 #endif
