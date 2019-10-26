9e2a64d32fa69e33bb3094800a86d8af1019729c
CutiePizza <ines.chokri97@gmail.com>
2019-10-26 21:29:02 +0000

    ok

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,13 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 #include <stdarg.h>
+
 /**
-   * struct _types - Struct types
-    *
-     * @s: The type
-      * @p: The function associated
-       */
+ * struct _types - Struct types
+ *
+ * @s: The type
+ * @p: The function associated
+ */
 
 typedef struct _types
 {
@@ -18,8 +19,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/*void print_c(va_list c);
-void print_i(va_list i);
-void print_f(va_list f)
-void print_s(va_list s);*/
 #endif
