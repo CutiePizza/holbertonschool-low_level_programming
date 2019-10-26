8a969198dd3853052f91a6e7784fa283cd33abe8
CutiePizza <ines.chokri97@gmail.com>
2019-10-26 23:32:47 +0000

    fixed

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -4,10 +4,13 @@ message db "Hello, Holberton", 10
 
 section .text
 
-global _start
-_start:
-mov rax, 1
-mov rdi, 1
-mov rsi, message
-mov rdx, 14
-syscall
+global main
+main:
+mov edx, 17
+mov ecx, message
+mov ebx, 1
+mov eax, 4
+int 0x80
+
+mov eax, 1
+int 0x80
