1797ac30aba922f0296abad66132ca384b202c76
CutiePizza <ines.chokri97@gmail.com>
2019-10-31 14:42:54 +0000

    pushing task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * list_len - Return number of elements of a list
+  * @h: header of the list
+  * Return: An unsigned integer
+  */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *p = h;
+	unsigned int i = 0;
+
+	while (p != NULL)
+	{
+		p = p->next;
+		i++;
+	}
+
+return (i);
+}
