55746c09212792ce0e7d9873df9ccec91cd16213
CutiePizza <ines.chokri97@gmail.com>
2019-10-31 20:08:43 +0000

    lists.h modified and task 100 added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print - print a line beofre main executin
+  */
+void  print(void)
+{
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
+}
+
+
+
