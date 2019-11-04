39d8f0147e04e1896d90e0431d654bac045d1a10
CutiePizza <ines.chokri97@gmail.com>
2019-11-04 18:44:38 +0000

    task 100 done

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverse a list
+  * @head: head of list
+  * Return: new list
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
