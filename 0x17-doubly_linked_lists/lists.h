7c150f16653438d9ce6756745a3ab199c3d75889
CutiePizza <ines.chokri97@gmail.com>
2019-12-13 07:58:12 +0100

    added libraries

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
