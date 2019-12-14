da5364681f0aecdfa5a32a01d6ff36dfe293cd1c
CutiePizza <ines.chokri97@gmail.com>
2019-12-14 16:10:42 +0100

    fixed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new != NULL)
 	{
 		new->n = n;
+		if (*head != NULL)
+			(*head)->prev = new;
 		new->next = (*head);
 		new->prev = NULL;
 		(*head) = new;
