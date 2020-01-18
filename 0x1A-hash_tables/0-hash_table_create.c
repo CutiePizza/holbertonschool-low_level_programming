2002058d0e37ca5d57e47814fada7c3cfd8eccd6
CutiePizza <ines.chokri97@gmail.com>
2020-01-18 20:39:57 +0100

    added test caset

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,12 +5,14 @@
  * @size: unisgned long int
  * Return: pointer to newly created hash table
  */
- hash_table_t *hash_table_create(unsigned long int size)
- {
-	 hash_table_t *hash_tab;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_tab;
 
-	 hash_tab = malloc(sizeof(hash_tab) * size);
-	 if (hash_tab == NULL)
-	 	return NULL;
+	if (size <= 0)
+		return (NULL);
+	hash_tab = malloc(sizeof(hash_tab) * size);
+	if (hash_tab == NULL)
+		return (NULL);
 	return (hash_tab);
- }
+}
