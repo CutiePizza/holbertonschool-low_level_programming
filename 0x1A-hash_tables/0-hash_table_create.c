c9ee7d92d3187bd042c94692d83429e534cabdf8
CutiePizza <ines.chokri97@gmail.com>
2020-01-18 21:55:54 +0100

    initialized a node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_tab;
+	hash_table_t *hash_tab = NULL;
 
 	if (size <= 0)
 		return (NULL);
 	hash_tab = malloc(sizeof(hash_tab) * size);
 	if (hash_tab == NULL)
 		return (NULL);
+	hash_tab->array = NULL;
+	hash_tab->size = 0;
 	return (hash_tab);
 }
