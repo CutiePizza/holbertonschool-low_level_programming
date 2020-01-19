87613b7e15ac9e05fe1bce26be852ee9d6920199
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 18:59:48 +0100

    removed tests on size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_tab = NULL;
 
-	if (size <= 0 || !size)
-		return (NULL);
 	hash_tab = malloc(sizeof(hash_table_t));
 	if (hash_tab == NULL)
 		return (NULL);
