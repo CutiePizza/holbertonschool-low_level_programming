28d32bc542237d11f8cadc7898a9ea3f9d32ea4e
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 22:21:31 +0100

    verify if its null first

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *temp;
 	unsigned long int index;
 
-	if (!key[0] || key == NULL || !ht || value == NULL || ht->array == NULL)
+	if (!key || !key[0] || !ht || value == NULL || ht->array == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
