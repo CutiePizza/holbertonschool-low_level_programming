8f97ccb97eb298ceecf00950771893f0b5702f99
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 19:33:54 +0100

    put test on size back

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_tab;
 
+	if (size <= 0)
+		return (NULL);
 	hash_tab = malloc(sizeof(hash_table_t));
 	if (hash_tab == NULL)
 		return (NULL);
-	hash_tab->size = size;
-	hash_tab->array = malloc(sizeof(hash_node_t) * size);
+	hash_tab->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_tab->array == NULL)
 	{
 		free(hash_tab);
 		return (NULL);
 	}
+	hash_tab->size = size;
 	return (hash_tab);
 }
