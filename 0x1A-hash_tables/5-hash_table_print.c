c4b3ce4e30676a751008d065882ec50fa345d15a
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 23:24:04 +0100

    task 5 done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print element of hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned int index = 0;
+	char *v = "";
+
+	if (!ht)
+		return;
+	printf("{");
+	while (index <= ht->size)
+	{
+		temp = ht->array[index];
+		while (temp != NULL)
+		{
+			printf("%s", v);
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			v = ", ";
+		}
+		index++;
+	}
+	printf("}\n");
+}
