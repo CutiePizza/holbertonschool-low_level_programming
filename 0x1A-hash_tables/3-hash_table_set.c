ce46f0c2224dc4b08726279fd20625b91928f6c8
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 21:15:52 +0100

    task 3 done

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table or updates it
+ * @ht: hash table
+ * @key: key to add or update
+ * @value: value associated to key
+ * Return: 1 on success, 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int index;
+
+	if (!ht || !key)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL)
+	{
+		while (ht->array[index])
+		{
+			if (strcmp(ht->array[index]->key, key) == 0)
+			{
+				ht->array[index]->value = strdup(value);
+				return (1);
+			}
+			ht->array[index] = ht->array[index]->next;
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
