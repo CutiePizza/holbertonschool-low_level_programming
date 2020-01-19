da7ba8e8269f683a7d39aee03d95a39a366875b8
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 21:26:55 +0100

    deleted test on hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	unsigned long int index;
 
-	if (!ht || !key)
+	if (!key)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL)
