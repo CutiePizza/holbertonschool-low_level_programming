b03ee7d9fe2f0cccd6c7ec55d2df4f17c53ca7e3
CutiePizza <ines.chokri97@gmail.com>
2020-02-23 20:43:30 +0100

    task 5 done

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,4 +8,14 @@ def island_perimeter(grid):
     """
     returns the island perimeter described in grid
     """
-
+    sums = 0
+    my_list = []
+    for i in range(len(grid)):
+        for j in grid[i]:
+            if j == 1:
+                my_list.append(j)
+    for i in range(1, len(my_list) - 1):
+        sums += 1
+    sums *= 2
+    sums += 6
+    return (sums)
