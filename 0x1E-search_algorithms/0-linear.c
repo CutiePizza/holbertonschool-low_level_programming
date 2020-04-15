83ae1d486977bb6face5d7b81d1d5e4f0e19c46d
CutiePizza <ines.chokri97@gmail.com>
2020-04-15 01:50:43 +0100

    task 0 done

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to find
+ * Return: first index of value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+return (-1);
+}
