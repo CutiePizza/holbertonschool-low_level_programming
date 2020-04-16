43b135b3d1f453dcdd66aeeaa72fd7d182110474
CutiePizza <ines.chokri97@gmail.com>
2020-04-16 12:49:28 +0100

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary search
+ * @array: pointer to an array of integers
+ * @size: size of array
+ * @value: value to look for
+ * Return: -1 if value nor found, Indew's value if found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+
+	size_t i, mid, left = 0, right = size - 1;
+
+
+	if (array == NULL)
+		return (-1);
+	while (left < right)
+	{
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[right]);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	printf("%d\n", array[left]);
+	return (-1);
+}
